a4f7894a7c9ab05170d9004cf5523b0789c34492
daoyuly <liudaoyu@gmail.com>
2014-01-04 17:35:04 +0800

    first commit

---
+++ DrawEngin/Element/IEle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DrawEngin.Style;
+
+namespace DrawEngin.Element
+{
+    class IEle : EleBase
+    {
+        public IEle()
+        {
+            this.Tag = "i";
+            this.Style = new StyleInfo(Display.Inline, FontStyle.Italic);
+        }
+    }
+}
